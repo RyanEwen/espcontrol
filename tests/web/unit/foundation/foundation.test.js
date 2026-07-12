e78aab459a400e55229a3f23ded8b6236e2374ee
jtenniswood <james@tenniswood.co.uk>
2026-07-12 17:29:54 +0100

    Route nested unit and mutation tests

--- tests/web/unit/foundation/foundation.test.js
+++ tests/web/unit/foundation/foundation.test.js
@@ -1,6 +1,6 @@
 const assert = require("node:assert/strict");
 const test = require("node:test");
 
-test("the Node test runner executes repository unit tests", () => {
+test("the Node test runner discovers nested repository unit tests", () => {
   assert.deepEqual(["firmware", "web"].sort(), ["web", "firmware"].sort());
 });
